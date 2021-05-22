02db30c60e8d9d99ce79155f3be89c27862665bc
charicodazzi <charicodazzi@hotmail.com>
2021-05-21 21:05:50 -0300

    switch

---
+++ app.js
@@ -0,0 +1,48 @@
+const inputCantidad= document.getElementById("cantidad");
+const tipoDeCambio= document.getElementById("tipodecambio");
+const resultado=document.getElementById("resultado");
+
+
+
+// Si lo hicieramos con condicionales, sin switch:
+/*
+const calculoDeMoneda = () =>{
+    if(tipoDeCambio.value === 'usd'){
+        resultado.textContent = inputCantidad.value * 0.011;
+    }else if(tipoDeCambio.value === 'mxn'){
+        resultado.textContent = inputCantidad.value * 0.21;
+    }else if(tipoDeCambio.value === 'col'){
+        resultado.textContent = inputCantidad.value * 39.78;
+    }else if(tipoDeCambio.value === 'pen'){
+        resultado.textContent = inputCantidad.value * 0.040;
+    }else{
+        resultado = "";
+    }
+    };
+    
+    tipoDeCambio.addEventListener('input', calculoDeMoneda);
+    inputCantidad.addEventListener('input', calculoDeMoneda);
+    */
+
+    const calculoDeMoneda = () =>{
+        switch (tipoDeCambio.value){
+        case 'usd':
+            resultado.textContent = inputCantidad.value * 0.011;
+            break;
+        case 'mxn':
+            resultado.textContent = inputCantidad.value * 0.21;
+            break;
+        case 'col':
+            resultado.textContent = inputCantidad.value * 39.78;
+            break;
+        case 'pen':
+            resultado.textContent = inputCantidad.value * 0.040;
+            break;
+        default:
+            resultado = "";
+        }
+        }
+        
+        tipoDeCambio.addEventListener('input', calculoDeMoneda);
+    inputCantidad.addEventListener('input', calculoDeMoneda);
+    
